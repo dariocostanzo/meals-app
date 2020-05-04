7a5cb4779bca8ee108ef8bc06b05422d67bc9da8
dariocostanzo <darioc@xara.com>
2020-05-04 09:52:59 +0100

    Adds filter switches

--- screens/FiltersScreen.js
+++ screens/FiltersScreen.js
@@ -1,20 +1,84 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
-
+import React, { useState, useEffect, useCallback } from 'react';
+import { View, Text, StyleSheet, Switch, Platform } from 'react-native';
 import { HeaderButtons, Item } from 'react-navigation-header-buttons';
+
 import HeaderButton from '../components/HeaderButton';
+import Colors from '../constants/Colors';
+
+const FilterSwitch = props => {
+  return (
+    <View style={styles.filterContainer}>
+      <Text>{props.label}</Text>
+      <Switch
+        trackColor={{ true: Colors.primaryColor }}
+        thumbColor={Platform.OS === 'android' ? Colors.primaryColor : ''}
+        value={props.state}
+        onValueChange={props.onChange}
+      />
+    </View>
+  );
+};
 
 const FiltersScreen = props => {
+  // To avoid unnecessary re-renders whenever anything in the param compomnent changes
+  // use object destructuring: props is an object and this pulls out the navigation key
+  // and stores this in a brand new constant of the same name, so of the same navigation.
+  // It stores the value in the navigation prop in that navigation constant
+  // The advantage is that now we have a navigation constant, which we can use in useEffect without props
+  const { navigation } = props;
+
+  const [isGlutenFree, setIsGlutenFree] = useState(false);
+  const [isLactoseFree, setIsLactoseFree] = useState(false);
+  const [isVegan, setIsVegan] = useState(false);
+  const [isVegetarian, setIsVegetarian] = useState(false);
+
+  const saveFilters = useCallback(() => {
+    const appliedFilters = {
+      glutenFree: isGlutenFree,
+      lactoseFree: isLactoseFree,
+      vegan: isVegan,
+      vegetarian: isVegetarian
+    };
+
+    console.log(appliedFilters);
+    //this component function (saveFilters) will be recreated inf any of the states below changes
+  }, [isGlutenFree, isLactoseFree, isVegan, isVegetarian]);
+
+  useEffect(() => {
+    // point save at the saveFilters function
+    props.navigation.setParams({ save: saveFilters });
+  }, [saveFilters]);
+
   return (
     <View style={styles.screen}>
-      <Text>FiltersScreen</Text>
+      <Text style={styles.title}>Available Filters / Restrictions</Text>
+      <FilterSwitch
+        label='Gluten-Free'
+        state={isGlutenFree} //forward
+        onChange={newValue => setIsGlutenFree(newValue)} //update
+      />
+      <FilterSwitch
+        label='Lactose-Free'
+        state={isLactoseFree}
+        onChange={newValue => setIsLactoseFree(newValue)}
+      />
+      <FilterSwitch
+        label='Vegan'
+        state={isVegan}
+        onChange={newValue => setIsVegan(newValue)}
+      />
+      <FilterSwitch
+        label='Vegetarian'
+        state={isVegetarian}
+        onChange={newValue => setIsVegetarian(newValue)}
+      />
     </View>
   );
 };
 
 FiltersScreen.navigationOptions = navData => {
   return {
-    headerTitle: 'Your Filters',
+    headerTitle: 'Filter Meals',
     headerLeft: () => (
       //burger button
       <HeaderButtons HeaderButtonComponent={HeaderButton}>
@@ -26,6 +90,23 @@ FiltersScreen.navigationOptions = navData => {
           }}
         ></Item>
       </HeaderButtons>
+    ),
+    headerRight: () => (
+      //burger button
+      <HeaderButtons HeaderButtonComponent={HeaderButton}>
+        <Item
+          title='Menu'
+          iconName='ios-save'
+          onPress={
+            // retrive sava parameter set inside useEffect
+            // using params to communicate between component and navigation option
+
+            // thanks to destructuring object we don't need to use navData (see above)
+            // navData.navigation.getParam('save');
+            navData.navigation.getParam('save')
+          }
+        ></Item>
+      </HeaderButtons>
     )
   };
 };
@@ -33,8 +114,21 @@ FiltersScreen.navigationOptions = navData => {
 const styles = StyleSheet.create({
   screen: {
     flex: 1,
-    justifyContent: 'center',
+    // justifyContent: 'center', vertical centering
     alignItems: 'center'
+  },
+  title: {
+    fontFamily: 'open-sans-bold',
+    fontSize: 22,
+    margin: 20,
+    textAlign: 'center'
+  },
+  filterContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    width: '80%',
+    marginVertical: 10
   }
 });
 
