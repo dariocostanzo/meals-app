5e869d0f0076854faa89ce3130f2c1db0db0ceba
dariocostanzo <darioc@xara.com>
2020-05-04 15:53:01 +0100

    Adds a message when there are no favorites

--- screens/FavoritesScreen.js
+++ screens/FavoritesScreen.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import { HeaderButtons, Item } from 'react-navigation-header-buttons';
+import { View, Text, StyleSheet } from 'react-native';
 //1. Redux - Import useSelector
 import MealList from '../components/MealList';
 import HeaderButton from '../components/HeaderButton';
+import DefaultText from '../components/DefaultText';
 import { useSelector } from 'react-redux';
 
 const FavoritesScreen = props => {
@@ -11,6 +13,14 @@ const FavoritesScreen = props => {
   const favMeals = useSelector(state => state.meals.favoriteMeals);
   // 3. We don't need to filter MEALS, we can just get them from availableMeals
   // const favMeals = MEALS.filter(meal => meal.id === 'm1' || meal.id === 'm2');
+
+  if (favMeals.length === 0 || !favMeals) {
+    return (
+      <View style={styles.content}>
+        <DefaultText>No favorite meals found. Start adding some!</DefaultText>
+      </View>
+    );
+  }
   return <MealList listData={favMeals} navigation={props.navigation} />;
 };
 
@@ -31,5 +41,13 @@ FavoritesScreen.navigationOptions = navData => {
     )
   };
 };
+
+const styles = StyleSheet.create({
+  content: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+});
 
 export default FavoritesScreen;
